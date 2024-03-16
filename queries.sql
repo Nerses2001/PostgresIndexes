f7048f629bb43e55652f0373991ee3e68e729c3d
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2024-03-16 13:50:06 -0800

    select range result

--- queries.sql
+++ queries.sql
@@ -13,3 +13,9 @@ explain
     select id, name
     from users
     where name = 'test1';
+
+explain
+    (analyse, costs  off ,buffers )
+    select id
+    from users
+    where id >= 5 and id <= 9;
